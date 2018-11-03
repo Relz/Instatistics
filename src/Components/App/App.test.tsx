54801ac9bde54b8b38c7052bcd9db20983cbd281
Starovoytov Alexey <relz0071@gmail.com>
2018-11-03 17:25:18 +0300

    Удалён тест компонента App

--- src/Components/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow, ShallowWrapper } from 'enzyme';
-import * as React from 'react';
-import { AppConnected } from './App';
-
-describe('AppConnected component', () => {
-	let appComponent: ShallowWrapper<undefined, undefined>;
-	beforeEach(() => (appComponent = shallow(<AppConnected />)));
-	it('Should render without error', () => expect(appComponent.length).toBe(1));
-});
